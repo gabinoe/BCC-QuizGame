04694f4d9624d1d6fd6996f28f0b80bfbb4eb59c
gabinoe <118677791+gabinoe@users.noreply.github.com>
2023-07-06 18:33:12 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,118 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const questionElement = document.getElementById('question');
-    const answerElement = document.getElementById('answers');
-    const timerElement = document.getElementById('timer');
-    const startButton = document.getElementById('start-btn');
-    const scoreContainer = document.getElementById('score-container');
-    const scoreElement = document.getElementById('score');
-    const initialsInput = document.getElementById('initials');
-    const saveButton = document.getElementById('save-btn');
+const quizContainer = document.getElementById("quiz-container");
+const startBtn = document.getElementById("start-btn");
+const questionElement = document.getElementById("question");
+const choicesElement = document.getElementById("choices");
+const resultContainer = document.getElementById("result-container");
+const resultElement = document.getElementById("result");
+const initialsForm = document.getElementById("initials-form");
+const initialsInput = document.getElementById("initials");
+const submitBtn = document.getElementById("submit-btn");
+const timerElement = document.getElementById("timer");
 
-    let currentQuestionIndex = 0;
-    let score = 0;
-    let timer = 30;
-    let timerInterval;
+let currentQuestionIndex = 0;
+let timeLeft = 60;
+let timerInterval;
 
-    startButton.addEventListener('click', startQuiz);
-    saveButton.addEventListener('click', saveScore);
+// Quiz questions
+const questions = [
+  {
+    question: "What is the capital of France?",
+    choices: ["London", "Paris", "Rome", "Berlin"],
+    answer: 1
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    choices: ["Mars", "Venus", "Mercury", "Jupiter"],
+    answer: 0
+  },
+  // Add more questions...
+];
 
-    function startQuiz() {
-        startButton.style.display = 'none';
-        scoreContainer.style.display = 'none';
-        loadQuestion();
-        startTimer();
+// Initialize the quiz
+function startQuiz() {
+  startBtn.classList.add("hide");
+  quizContainer.classList.remove("hide");
+  startTimer();
+  showQuestion();
+}
+
+// Start the timer
+function startTimer() {
+  timerInterval = setInterval(function() {
+    timeLeft--;
+    timerElement.textContent = timeLeft;
+
+    if (timeLeft <= 0) {
+      endQuiz();
     }
+  }, 1000);
+}
 
-    function loadQuestion() {
-        const question = questions[currentQuestionIndex];
-        questionElement.innerText = question.question;
+// Display a question and its choices
+function showQuestion() {
+  const question = questions[currentQuestionIndex];
+  questionElement.textContent = question.question;
+  choicesElement.innerHTML = "";
 
-        answerElement.innerHTML = '';
-        for (let i = 0; i < question.answers.length; i++) {
-            const answer = document.createElement('li');
-            answer.innerText = question.answers[i];
-            answer.addEventListener('click', checkAnswer);
-            answerElement.appendChild(answer);
-        }
-    }
+  question.choices.forEach(function(choice, index) {
+    const choiceBtn = document.createElement("button");
+    choiceBtn.classList.add("choice");
+    choiceBtn.textContent = choice;
+    choiceBtn.addEventListener("click", checkAnswer);
+    choicesElement.appendChild(choiceBtn);
+  });
+}
 
-    function checkAnswer() {
-        const selectedAnswer = this.innerText;
-        const question = questions[currentQuestionIndex];
+// Check if the selected answer is correct
+function checkAnswer(event) {
+  const selectedChoice = event.target;
+  const question = questions[currentQuestionIndex];
 
-        if (selectedAnswer === question.correctAnswer) {
-            score++;
-        } else {
-            timer -= 5;
-        }
+  if (question.answer === Array.from(choicesElement.children).indexOf(selectedChoice)) {
+    resultElement.textContent = "Correct!";
+  } else {
+    resultElement.textContent = "Incorrect!";
+    timeLeft -= 10;
+  }
 
-        currentQuestionIndex++;
+  resultContainer.classList.remove("hide");
+  setTimeout(function() {
+    resultContainer.classList.add("hide");
+  }, 1000);
 
-        if (currentQuestionIndex
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex === questions.length) {
+    endQuiz();
+  } else {
+    showQuestion();
+  }
+}
+
+// End the quiz
+function endQuiz() {
+  clearInterval(timerInterval);
+  quizContainer.classList.add("hide");
+  resultContainer.classList.add("hide");
+
+  resultElement.textContent = `Your score is ${timeLeft}`;
+  initialsInput.value = "";
+  initialsInput.focus();
+  resultContainer.classList.remove("hide");
+}
+
+// Save the initials and score
+function saveScore(event) {
+  event.preventDefault();
+  const initials = initialsInput.value.trim();
+
+  // Save the initials and score using an API or local storage
+  console.log(`Initials: ${initials}, Score: ${timeLeft}`);
+
+  // Clear the form
+  initialsInput.value = "";
+}
+
+// Event listeners
+startBtn.addEventListener("click", startQuiz);
+initialsForm.addEventListener("submit", saveScore);
